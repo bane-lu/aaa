f9e7dcc4beabb3fb4e50ca2e30af625f85287a1a
zhouziqi <zqzhoui@isoftstone.com>
2018-05-16 17:13:48 +0800

    alert url

--- js/get8g.js
+++ js/get8g.js
@@ -14,13 +14,14 @@
     submitMobile: '/check_mobile/app/submitMobile/',
     checkMobile: '/check_mobile/app/checkMobile/'
   }
-  alert("version:16:41")
+  // alert("version:16:41")
 
    //token状态
    var hasToken = 2;    // 0 拿到token 且为广东用户     1  拿到token 非广东用户        2没拿到token
   // 客户端token的拿取 */
   function getToken(){
     var url = window.location.href; //获取url中"?"符后的字串
+    alert(url)
     var theRequest = new Object();
     var n = url.indexOf("?")
     if (n != -1) {
@@ -161,7 +162,7 @@ get_btn.addEventListener('touchend', e => {
 const goto = document.getElementsByClassName("goto")[0]
 goto.addEventListener(clickEvent, e => {
     // document.location = '../download/index.html'
-    window.location.href= "http://feixin.10086.cn/miyou/"
+    window.location.href= basePath + "/advertises/download/index.html"
 })
 // 取消
 const cancel = document.getElementsByClassName("cancel")[0]
