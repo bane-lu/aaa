0516d05d05dc54d6aaa3b41ad49f488697d1bf82
zhouziqi <zqzhoui@isoftstone.com>
2018-07-04 15:02:10 +0800

    通话送话费-web

---
+++ ad_sendFee_web/js/seedFee.js
@@ -0,0 +1,105 @@
+// 
+
+var DEVICE = phonetype();
+var CHANNEL = getUrlParam('channel')||'m30100087';
+$(function () {
+
+  setpoint("visitor");
+
+  getDevice();
+  catch_ios()
+  //IOS埋点
+  function catch_ios() {
+    var u = navigator.userAgent;
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    if (isiOS) {
+      setpoint("download");
+    }
+  }
+
+});
+//埋点
+function setpoint(type) {
+  var device = DEVICE;
+  var channel = CHANNEL;
+  var phone = getPhone();
+  // alert(phone);
+  var paramData = {
+    "systemType": device,
+    "channel": channel,
+    "operatorType": type,
+    "mobileNumber": phone
+  };
+  $.ajax({
+    type: "get",
+    url: "http://rcsoa-nopay.zone139.com/versionmanager/download/meetyou-record",
+    // url: 'http://221.176.34.113:9000/versionmanager/download/meetyou-record',
+    data: $.param(paramData),
+    dataType: 'json',
+    success: function (res) {
+      console.log(res);
+    },
+    error: function (res) {
+      // console.log(res);
+      // alert("error")
+    }
+  });
+
+}
+/**
+ * getUrlParam   获取url传参
+ * @param name
+ * @return
+ */
+function getUrlParam(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+  var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+  if (r != null) return r[2];
+  return null; //返回参数值
+}
+/**
+ * 判断手机类型
+ */
+function phonetype() {
+  var ua = navigator.userAgent.toLowerCase();
+  var DEVICE = "";
+  if (/(iphone|ipod|ipad);?/i.test(ua)) {
+    DEVICE = "ios";
+  } else if (/android|adr/.test(ua) && !(/windows phone/.test(ua))) {
+    DEVICE = "android";
+  }
+  return DEVICE;
+}
+//判断是安卓还是ios
+function getDevice() {
+  var u = navigator.userAgent;
+  var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+  var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+  if (isAndroid) {
+  } else if (isiOS) {
+  }
+}
+//获取手机号
+function getPhone() {
+  var phone = ''
+  $.ajax({
+    url: 'http://120.197.89.223/app/phoneget.do',
+    type: "get",
+    data: {},
+    async: false,
+    success: function (data) {
+      phone = data;
+    }
+  });
+  return phone;
+}
+
+//识别浏览器是否为微信
+function isWeixin() {
+  var ua = navigator.userAgent.toLowerCase();
+  if (ua.match(/MicroMessenger/i) == "micromessenger") {
+    return true;
+  } else {
+    return false;
+  }
+}
