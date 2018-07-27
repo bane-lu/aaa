68754a84c7bc8a5ff1026888f3801bae6e5af3d6
linzejie <abcd1234>
2018-07-27 14:35:26 +0800

    修改弹框bug

--- promote8g/js/index.js
+++ promote8g/js/index.js
@@ -3,10 +3,18 @@ $(function () {
         $(".rule").show();
         $(".mask").show();
         $(".main").addClass("hidden");
+        
+        $(".main").height($(".main").height() + 1);
+        window.scrollTo(0, 1);
     });
     $(".rule img").on("touchstart", function () {
         $(".rule").hide();
         $(".mask").hide();
         $(".main").removeClass("hidden");
+        $(".main").height($(".main").height() - 1);
+        window.scrollTo(0, 0);
+    })
+    $("..rule .details").on("touchstart",function(){
+        window.scrollTo(0, 1);
     })
 })
