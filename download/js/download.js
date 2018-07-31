d5f62886dd31a08a2835173d4a28211ab0b6a119
zhouziqi <zqzhoui@isoftstone.com>
2018-07-31 10:18:02 +0800

    官网下载加插码

--- download/js/download.js
+++ download/js/download.js
@@ -35,13 +35,18 @@ $(function () {
 
   //点击下载
   $(".download-btn").on("click", function (e) {
-    // e.preventDefault();
+      $(".pv_btn").remove();
+      $(".wrapper").append('<div class="pv_btn" style="visibility: hidden"></div>')
+      $(".wrapper").find(".pv_btn").html("<script type='text/javascript' src='"+pv_btn_set(PDD)+"' />")
+
     if (isWeixin()) {
       //是微信
       e.preventDefault();
       $(".popup").show();
       $(".popup,.mask").fadeIn();
+
     } else {
+
       var u = navigator.userAgent;
       var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
       if (isiOS) {
@@ -49,13 +54,25 @@ $(function () {
       }
     }
   })
-  // $(window).resize(function () {
-  //   window.location.reload();
-  // })
 
 });
+
+var PDD = {};
 var DEVICE = phonetype();
 var CHANNEL = getUrlParam("channel");
+console.log(CHANNEL)
+
+// 判断插码页面
+function differPV(){
+  for(var i=0; i < pvData.length; i ++){
+    if(pvData[i].channel == CHANNEL){
+      PDD = pvData[i]
+      pv_set(PDD)
+    }
+  }
+}
+differPV()
+
 //埋点
 function setpoint(type) {
   var device = DEVICE;
@@ -77,10 +94,10 @@ function setpoint(type) {
     dataType: 'json',
     success: function (res) {
       // alert("success")
-      console.log(res);
+      // console.log(res);
     },
     error: function (res) {
-      console.log(res);
+      // console.log(res);
       // alert("error")
     }
   });
@@ -125,6 +142,25 @@ function phonetype() {
   return DEVICE;
 }
 
+// PV跟踪 插码
+function pv_set(pd){
+  var appid = pd.appid;
+  var label = pd.label;
+  var event = "AdExposed";
+  var src="http://120.197.233.121/udata/u.gif?h="+document.body.clientHeight+"&w="+document.body.clientWidth+"&ct="+new Date().getTime()+"&si="+appid+"&cu="+encodeURIComponent(window.location.host)+"&v=1.0&s=1500347894640218363&f=3&c=1428456744583&et="+event+"&lv="+ encodeURIComponent(label)+"&cp="+encodeURIComponent(window.location.href);
+
+  $("#PV").html("<script type='text/javascript' src='"+src+"' />");  
+}
+
+// 点击事件跟踪
+function pv_btn_set(pd){
+  var appid = pd.appid;
+  var label = pd.label;
+  var event = pd.adEvent;
+  var ru = pd.ru;
+  var src="http://120.197.233.121/udata/u.gif?h="+document.body.clientHeight+"&w="+document.body.clientWidth+"&ct="+new Date().getTime()+"&si="+appid+"&cu="+encodeURIComponent(window.location.host)+"&v=1.0&s=1500347894640218363&f=4&c=1428456744583&et="+event+"&lv="+ encodeURIComponent(label);
+  return src
+}
 /**
  * 图片进行预加载
  * **/
---
+++ download/js/pvData.js
@@ -0,0 +1,18 @@
+const pvData = [
+  // 和飞信触屏版门户首页-娱乐模块文字链
+  {	
+  	channel: 'm30100118',
+    appid: '300011860393',
+    label: '13658@@65338@@22245',
+    adEvent: 'ADClick1',
+    ru: 'http://feixin.10086.cn/miyou/index.html?channel=m30100118',
+  },
+  // 和通讯录 活动广场互推专用1
+  { 
+    channel: 'm30100119',
+    appid: '300011860393',
+    label: '13657@@65338@@24466',
+    adEvent: 'ADClick1',
+    ru: 'http://feixin.10086.cn/miyou/index.html?channel=m30100119',
+  }
+]
