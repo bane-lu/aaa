7a25206a2c4266836f29f3cee092136202642d9a
zhouziqi <zqzhoui@isoftstone.com>
2018-08-09 17:07:49 +0800

    m30100120

--- ad_sendFee/js/seedFee.js
+++ ad_sendFee/js/seedFee.js
@@ -58,7 +58,7 @@ function add_channel(){
 var DEVICE = phonetype();
 var PDD = {};
 var ACTIVITY = getUrlParam('activity') || '';
-var CHANNEL = add_channel()||'m30100087';
+var CHANNEL = add_channel()||'m30100120';
 
 // 判断插码页面
 function differPV(){
