6e73a9eb52c7197edd26929f3317c344a67b04f8
qiuyuanzheng <19802021154@139.com>
2019-03-19 10:55:01 +0800

    调试

--- promotion/js/download.js
+++ promotion/js/download.js
@@ -64,10 +64,28 @@ $(function () {
             $(".popup,.mask").fadeIn();
 
         } else {
-            window.location.href = 'meetyou://';
-            window.setTimeout(function(){
-                window.location.href = url;
-            },2000)
+            var timeout, t = 1000, hasApp = true; 
+            setTimeout(function () { 
+                if (!hasApp) { 
+                    //没有安装支付宝
+                        window.location.href=url;
+                }else{
+                    //安装支付宝
+                }
+                document.body.removeChild(ifr); 
+            }, 2000) 
+            
+            var t1 = Date.now(); 
+            var ifr = document.createElement("iframe"); 
+            ifr.setAttribute('src', 'meetyou://'); 
+            ifr.setAttribute('style', 'display:none'); 
+            document.body.appendChild(ifr); 
+            timeout = setTimeout(function () { 
+                var t2 = Date.now(); 
+                if (!t1 || t2 - t1 < t + 100) { 
+                hasApp = false; 
+                } 
+            }, t); 
 
             // var u = navigator.userAgent;
             // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
