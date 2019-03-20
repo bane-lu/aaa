6d73db77265d2ff87af0b4c08821477b1574f683
qiuyuanzheng <19802021154@139.com>
2019-03-20 14:59:26 +0800

    调试

--- promotion/js/download.js
+++ promotion/js/download.js
@@ -2,6 +2,7 @@ $(function () {
     // var DEVICE = phonetype();
     var CHANNEL = getUrlParam("channel");
 
+    //window.location.href = 'meetyou://'; 
     setpoint("visitor");
 
     var u = navigator.userAgent;
@@ -30,28 +31,6 @@ $(function () {
     // }
 
     // getDevice();
-
-
-    //识别浏览器是否为微信
-    function isWeixin() {
-        var ua = navigator.userAgent.toLowerCase();
-        if (ua.match(/MicroMessenger/i) == "micromessenger") {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    if (isWeixin()) {
-        //是微信
-        e.preventDefault();
-        $(".popup").show();
-        $(".popup,.mask").fadeIn();
-
-    } else {
-        window.location.href = 'meetyou://'; 
-    }
-
     function pullApp() {
         if (isAndroid) {
             url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/' + CHANNEL;
@@ -68,6 +47,26 @@ $(function () {
             }, 2000)
             return
         }
+    }
+
+
+    //识别浏览器是否为微信
+    function isWeixin() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.match(/MicroMessenger/i) == "micromessenger") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (isWeixin()) {
+        //是微信
+        $(".popup").show();
+        $(".popup,.mask").fadeIn();
+
+    } else {
+        window.location.href = 'meetyou://';
     }
 
     // 点击效果
