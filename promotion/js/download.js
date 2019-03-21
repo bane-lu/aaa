cd9003fc852f6066a67a3a7f03bb0f6a0951a5f9
qiuyuanzheng <19802021154@139.com>
2019-03-21 09:57:40 +0800

    调试

--- promotion/js/download.js
+++ promotion/js/download.js
@@ -6,14 +6,19 @@ $(function () {
         url : '',
     }
     
-    window.location.href ="weixin://";
+    // window.location.href ="meetyou://";
     // var ifr = document.createElement('iframe');  
-    //     ifr.src = 'weixin://';  
+    //     ifr.src = 'meetyou://';  
     //     ifr.style.display = 'none';  
     //     document.body.appendChild(ifr);  
     //     window.setTimeout(function(){  
     //         document.body.removeChild(ifr);  
     //     },3000) 
+    var ifm = document.createElement('iframe');
+    ifm.frameborder = '0';
+    ifm.style.cssText = 'display:none;border:0;width:0;height:0;';
+    document.body.appendChild(ifm);
+    ifm.src = 'meetyou://';  
 
 
 
@@ -41,14 +46,14 @@ $(function () {
     function pullApp() {
         if (base.isAndroid) {
             base.url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/';
-            window.location.href ="weixin://";
+            window.location.href ="meetyou://";
             window.setTimeout(function() {
                 window.location.href = base.url;
             }, 2000)
             return
         } else if (base.isiOS) {
             base.url = 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8';
-            window.location.href ="weixin://";
+            window.location.href ="meetyou://";
             window.setTimeout(function() {
                 window.location.href = base.url;
             }, 2000)
