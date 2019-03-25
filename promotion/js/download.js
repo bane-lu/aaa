2c9aa43bf0917b6fb2a9465f6a837111c7ac9d97
qiuyuanzheng <19802021154@139.com>
2019-03-25 16:30:07 +0800

    适配方案修改

--- promotion/js/download.js
+++ promotion/js/download.js
@@ -1,4 +1,13 @@
 $(function () {
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    if(isAndroid){
+        window.location.href = "http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/"
+    }else if(isiOS){
+        window.location.href = 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"'
+    }
+
     //判断是安卓还是ios
     function getDevice() {
         var u = navigator.userAgent;
