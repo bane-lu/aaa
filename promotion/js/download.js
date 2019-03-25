bde8af9abfd6073193e194652fae1d816c2cd18e
qiuyuanzheng <19802021154@139.com>
2019-03-25 15:45:23 +0800

    ui修改

--- promotion/js/download.js
+++ promotion/js/download.js
@@ -1,65 +1,20 @@
 $(function () {
-    var base = {
-        u : navigator.userAgent,
-        isAndroid : navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Adr') > -1, //android终端
-        isiOS : !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
-        url : '',
-    }
-    
-    // window.location.href ="meetyou://";
-    // var ifr = document.createElement('iframe');  
-    //     ifr.src = 'meetyou://';  
-    //     ifr.style.display = 'none';  
-    //     document.body.appendChild(ifr);  
-    //     window.setTimeout(function(){  
-    //         document.body.removeChild(ifr);  
-    //     },3000) 
-    var ifm = document.createElement('iframe');
-    ifm.frameborder = '0';
-    ifm.style.cssText = 'display:none;border:0;width:0;height:0;';
-    document.body.appendChild(ifm);
-    ifm.src = 'meetyou://';  
-
-
-
     //判断是安卓还是ios
-    // function getDevice() {
-    //     var u = navigator.userAgent;
-    //     var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-    //     var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-    //     if (isAndroid) {
-    //         console.log("sdf");
-    //         // var url = 'http://221.176.34.113:9000/versionmanager/download/meetyou-release/' + CHANNEL;
-    //         // var url = 'http://rcsoa-nopay.zone139.com/versionmanager/download/meetyou-release/' + CHANNEL;
-    //         url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/' + CHANNEL;
-    //         // $(".btn-wrapper a").attr('href', url);
-    //         $(".buoy-wrapper a").attr('href', url);
-    //     } else if (isiOS) {
-    //         url = 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8'
-    //         // $(".btn-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
-    //         $(".buoy-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
-    //         // setpoint("download");
-    //     }
-    // }
-
-    // getDevice();
-    function pullApp() {
-        if (base.isAndroid) {
-            base.url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/';
-            window.location.href ="meetyou://";
-            window.setTimeout(function() {
-                window.location.href = base.url;
-            }, 2000)
-            return
-        } else if (base.isiOS) {
-            base.url = 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8';
-            window.location.href ="meetyou://";
-            window.setTimeout(function() {
-                window.location.href = base.url;
-            }, 2000)
-            return
+    function getDevice() {
+        var u = navigator.userAgent;
+        var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+        if (isAndroid) {
+            var url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/';
+            $(".head-wrapper a").attr('href', url);
+            $(".bottom-wrapper a").attr('href', url);
+        } else if (isiOS) {
+            $(".head-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
+            $(".bottom-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
         }
     }
+
+    getDevice();
 
 
     //识别浏览器是否为微信
@@ -86,149 +41,35 @@ $(function () {
         $('.buoy-wrapper a').css('opacity','1')
     })
     //点击下载
-    $("#download").on("click", function (e) {
+    $(".head-button,.bottom-button").on("click", function (e) {
         if (isWeixin()) {
             //是微信
             e.preventDefault();
             $(".popup").show();
             $(".popup,.mask").fadeIn();
 
-        } else {
-            pullApp();
-
-            // var u = navigator.userAgent;
-            // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-            // if (isiOS) {
-            //     setpoint("download");
-            // }
         }
-    })
-
-    // 浮标点击下载
-    $(".buoy").on("click", function (e) {
-        if (isWeixin()) {
-            //是微信
-            e.preventDefault();
-            $(".popup").show();
-            $(".popup,.mask").fadeIn();
-
-        } else {
-            pullApp();
-
-            // var u = navigator.userAgent;
-            // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-            // if (isiOS) {
-            //     setpoint("download");
-            // }
-        }
-    })
-
-    // document.getElementById('download').onclick = function(e){
-    //     console.log("2")
-    //     // 通过iframe的方式试图打开APP，如果能正常打开，会直接切换到APP，并自动阻止a标签的默认行为
-    //     // 否则打开a标签的href链接
-    //     var ifr = document.createElement('iframe');
-    //     ifr.src = 'meetyou://';
-    //     ifr.style.display = 'none';
-    //     document.body.appendChild(ifr);
-    //     window.setTimeout(function(){
-    //         document.body.removeChild(ifr);
-    //     },2000)
-    // };
-
-    
+    })    
 
 });
 
-// 浮标显示与隐藏
-$(window).load(function(){
-    var $win = $(window);
-    var itemOffsetTop = $(".btn-wrapper").offset().top;
-    var itemOuterHeight = $(".btn-wrapper").outerHeight();
-    var winHeight = $win.height();
-    $win.scroll(function () {
-        var winScrollTop = $win.scrollTop();
-        if(!(winScrollTop > itemOffsetTop+itemOuterHeight)&&!(winScrollTop < itemOffsetTop-winHeight)) {
-            $(".buoy").hide();
-        } else{
-            $(".buoy").show();
-        }
-    });
-    // window.onbeforeunload = reloadBuoy();
-    // function reloadBuoy(){
-    //     console.log('111');
-    //     var winScrollTop = $win.scrollTop();
-    //     if(!(winScrollTop > itemOffsetTop+itemOuterHeight)&&!(winScrollTop < itemOffsetTop-winHeight)) {
-    //         $(".buoy").hide();
-    //         console.log('222');
-    //     } else{
-    //         $(".buoy").show();
-    //     }
-    // };
-})
+// // 浮标显示与隐藏
+// $(window).load(function(){
+//     var $win = $(window);
+//     var itemOffsetTop = $(".btn-wrapper").offset().top;
+//     var itemOuterHeight = $(".btn-wrapper").outerHeight();
+//     var winHeight = $win.height();
+//     $win.scroll(function () {
+//         var winScrollTop = $win.scrollTop();
+//         if(!(winScrollTop > itemOffsetTop+itemOuterHeight)&&!(winScrollTop < itemOffsetTop-winHeight)) {
+//             $(".buoy").hide();
+//         } else{
+//             $(".buoy").show();
+//         }
+//     });
+// })
 
 // 遮罩层点击消失
 $('.mask').on("click",function(){
     $('.popup,.mask').fadeOut();
 })
-
-
-/**
- * getUrlParam   获取url传参
- * @param name
- * @return
- */
-function getUrlParam(name) {
-    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-    if (r != null) return r[2];
-    return null; //返回参数值
-}
-/**
- * 判断手机类型
- */
-function phonetype() {
-    var ua = navigator.userAgent.toLowerCase();
-    var DEVICE = "";
-    if (/(iphone|ipod|ipad);?/i.test(ua)) {
-        DEVICE = "ios";
-    } else if (/android|adr/.test(ua) && !(/windows phone/.test(ua))) {
-        DEVICE = "android";
-    }
-    return DEVICE;
-}
-
-// PV跟踪 插码
-function pv_set(pd){
-    var appid = pd.appid;
-    var label = pd.label;
-    var event = "AdExposed";
-    var src="http://120.197.233.121/udata/u.gif?h="+document.body.clientHeight+"&w="+document.body.clientWidth+"&ct="+new Date().getTime()+"&si="+appid+"&cu="+encodeURIComponent(window.location.host)+"&v=1.0&s=1500347894640218363&f=3&c=1428456744583&et="+event+"&lv="+ encodeURIComponent(label)+"&cp="+encodeURIComponent(window.location.href);
-
-    $("#PV").html("<script type='text/javascript' src='"+src+"' />");
-}
-
-// 点击事件跟踪
-function pv_btn_set(pd){
-    var appid = pd.appid;
-    var label = pd.label;
-    var event = "ADClick1";
-    var ru = pd.ru;
-    var src="http://120.197.233.121/udata/u.gif?h="+document.body.clientHeight+"&w="+document.body.clientWidth+"&ct="+new Date().getTime()+"&si="+appid+"&cu="+encodeURIComponent(window.location.host)+"&v=1.0&s=1500347894640218363&f=4&c=1428456744583&et="+event+"&lv="+ encodeURIComponent(label);
-    return src
-}
-/**
- * 图片进行预加载
- * **/
-// var images = new Array();
-// function preload() {
-//   for (var i = 0; i < preload.arguments.length; i++) {
-//     images[i] = new Image()
-//     images[i].src = preload.arguments[i]
-//   }
-// }
-//    preload(
-//      'img/pic_1@2x.png',
-//      'img/pic_2@2x.png',
-//      'img/pic_3@2x.png'
-//    )
