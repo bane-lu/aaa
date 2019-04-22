985bf4c9c28dce1081cafd86f720837c33e0834e
linzejie <abcd1234>
2019-04-22 18:07:20 +0800

    测试

--- download/js/download.js
+++ download/js/download.js
@@ -27,16 +27,11 @@ $(function () {
         $(".popup,.mask").fadeIn();
       }
     } else {
+      // setpoint("download");
       var u = navigator.userAgent;
-      var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
       var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-
-      if (isiOS) {
-        window.location.href = 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8'
-      } else {
-        // var url = 'http://rcsoa-nopay.zone139.com/versionmanager/download/meetyou-release/' + CHANNEL;
-        window.location.href = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/'
-      }
+      // if (isiOS) {
+      // }
     }
   })
 
