3de0be80660319164a6a60c506795c636145362b
qiuyuanzheng <19802021154@139.com>
2019-04-22 10:51:54 +0800

    优化，修改ios微信端点击下载跳转地址

--- promotion/js/download.js
+++ promotion/js/download.js
@@ -1,31 +1,4 @@
 $(function () {
-    // var u = navigator.userAgent;
-    // var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-    // var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-    // if(isAndroid){
-    //     window.location.href = "http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/"
-    // }else if(isiOS){
-    //     window.location.href = 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"'
-    // }
-
-    //判断是安卓还是ios
-    function getDevice() {
-        var u = navigator.userAgent;
-        var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-        var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-        if (isAndroid) {
-            var url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/';
-            $(".head-wrapper a").attr('href', url);
-            $(".bottom-wrapper a").attr('href', url);
-        } else if (isiOS) {
-            $(".head-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
-            $(".bottom-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
-        }
-    }
-
-    getDevice();
-
-
     //识别浏览器是否为微信
     function isWeixin() {
         var ua = navigator.userAgent.toLowerCase();
@@ -36,19 +9,27 @@ $(function () {
         }
     }
 
-    // 点击效果
-    // $('.btn-wrapper').on('touchstart',function(){
-    //     $('.btn-wrapper a').css('opacity','0.7')
-    // })
-    // $('.btn-wrapper').on('touchend',function(){
-    //     $('.btn-wrapper a').css('opacity','1')
-    // })
-    // $('.buoy-wrapper').on('touchstart',function(){
-    //     $('.buoy-wrapper a').css('opacity','0.7')
-    // })
-    // $('.buoy-wrapper').on('touchend',function(){
-    //     $('.buoy-wrapper a').css('opacity','1')
-    // })
+    var u = navigator.userAgent;
+    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+    var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+    //判断是安卓还是ios
+    function getDevice() {
+        if (isAndroid) {
+            var url = 'http://a.10086.cn/c/a/s.do?requestid=zndxzh&channelid=5410453499&cid=300011040393&gid=300011040393/';
+            $(".head-wrapper a").attr('href', url);
+            $(".bottom-wrapper a").attr('href', url);
+        } else if (isiOS) {
+            if(isWeixin()){
+                $(".head-wrapper a").attr("href", 'https://a.app.qq.com/o/simple.jsp?pkgname=com.cmic.college');
+                $(".bottom-wrapper a").attr("href", 'https://a.app.qq.com/o/simple.jsp?pkgname=com.cmic.college');
+            }else{
+                $(".head-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
+                $(".bottom-wrapper a").attr("href", 'itms-apps://itunes.apple.com/cn/app/%E5%AF%86%E5%8F%8B%E5%9C%88/id1266608463?mt=8"');
+            }
+        }
+    }
+    getDevice();
+
     //点击下载
     $(".head-button,.bottom-button").on("click", function (e) {
         if (isWeixin()) {
@@ -91,7 +72,6 @@ $(function () {
         }
         return { isAndroid, isIOS, isIOS9 }
     }
-    
     // 判断手机上是否安装了app，如果安装直接打开url，如果没安装，执行callback
     function openApp(url, callback) {
         let { isAndroid, isIOS, isIOS9 } = detectVersion()
@@ -130,26 +110,9 @@ $(function () {
     
         }
     }
-
     openApp("meetyou://")
 
 });
-
-// // 浮标显示与隐藏
-// $(window).load(function(){
-//     var $win = $(window);
-//     var itemOffsetTop = $(".btn-wrapper").offset().top;
-//     var itemOuterHeight = $(".btn-wrapper").outerHeight();
-//     var winHeight = $win.height();
-//     $win.scroll(function () {
-//         var winScrollTop = $win.scrollTop();
-//         if(!(winScrollTop > itemOffsetTop+itemOuterHeight)&&!(winScrollTop < itemOffsetTop-winHeight)) {
-//             $(".buoy").hide();
-//         } else{
-//             $(".buoy").show();
-//         }
-//     });
-// })
 
 // 遮罩层点击消失
 $('.mask').on("click",function(){
